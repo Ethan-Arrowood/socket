5fe380bd6085307b14e98ae6f5a07f96095687f7
Ethan Arrowood <ethan.arrowood@vercel.com>
2023-05-17 16:31:03 -0600

    implement everything but startTls

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
 import net from 'node:net';
+import type { ReadableStream, WritableStream } from 'node:stream/web';
 
 interface SocketOptions {
+  /** 
+   * Specifies whether or not to use TLS when creating the TCP socket.
+   * `off` — Do not use TLS.
+   * `on` — Use TLS.
+   * `starttls` — Do not use TLS initially, but allow the socket to be upgraded to use TLS by calling startTls().
+   */
   secureTransport: 'off' | 'on' | 'starttls';
+  /**
+   * Defines whether the writable side of the TCP socket will automatically close on end-of-file (EOF).
+   * When set to false, the writable side of the TCP socket will automatically close on EOF.
+   * When set to true, the writable side of the TCP socket will remain open on EOF.
+   * This option is similar to that offered by the Node.js net module and allows interoperability with code which utilizes it.
+   */
+  allowHalfOpen: boolean;
 }
 
 interface SocketAddress {
@@ -11,24 +25,79 @@ interface SocketAddress {
   port: number;
 }
 
-function connect(
+export function connect(
   address: SocketAddress | string,
   options?: SocketOptions,
 ): Socket {
-  return new Socket('', options);
+  if (typeof address === 'string') {
+    const url = new URL(address);
+    // eslint-disable-next-line no-param-reassign
+    address = {
+      hostname: url.hostname,
+      port: parseInt(url.port)
+    }
+  }
+  return new Socket(address, options);
 }
 
-class Socket {
+export class Socket {
+
+  socket: net.Socket;
   readable: ReadableStream;
   writable: WritableStream;
   closed: Promise<void>;
-  constructor(hostPort: string, options?: SocketOptions) {
-    this.readable = new ReadableStream();
-    this.writable = new WritableStream();
-    this.closed = new Promise(() => {});
+
+  private closedResolved = false;
+  private closedRejected = false;
+  private closedResolve!: () => void;
+  private closedReject!: (reason?: any) => void;
+
+  constructor(address: SocketAddress, options: SocketOptions = { secureTransport: "off", allowHalfOpen: true }) {
+
+    this.closed = new Promise((resolve, reject) => {
+      this.closedResolve = (): void => {
+        this.closedResolved = true;
+        resolve();
+      };
+      this.closedReject = (): void => {
+        this.closedRejected = true;
+        reject();
+      }
+    });
+
+    this.socket = new net.Socket({
+      allowHalfOpen: options.allowHalfOpen
+    })
+
+    this.socket.on('close', (hadError) => {
+      if (!hadError && !this.closedFulfilled && !this.closedResolved) {
+        this.closedResolve();
+      }
+    })
+
+    this.socket.on('error', (err) => {
+      if (!this.closedFulfilled && !this.closedRejected) {
+        this.closedReject(err);
+      }
+    })
+
+    // types are wrong. fixed based on docs https://nodejs.org/dist/latest-v19.x/docs/api/stream.html#streamduplextowebstreamduplex
+    const { readable, writable } = net.Socket.Duplex.toWeb(this.socket) as unknown as { readable: ReadableStream, writable: WritableStream};
+    this.readable = readable;
+    this.writable = writable;
+
+    this.socket.connect(address)
+
   }
-  async close(): Promise<void> {}
-  startTls(): Socket {
-    return new Socket('');
+  get closedFulfilled (): boolean {
+    return this.closedResolved || this.closedRejected
   }
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async close(): Promise<void> {
+    this.socket.destroy();
+    this.closedResolve();
+  }
+  // TODO
+  // startTls(): Socket {
+  // }
 }
