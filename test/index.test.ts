774edf7ad3daf2403166352abe7ccc04ae99835d
flakey5 <73616808+flakey5@users.noreply.github.com>
2023-09-15 11:09:32 -0700

    test: add tests for read/write binary data

--- test/index.test.ts
+++ test/index.test.ts
@@ -87,3 +87,241 @@ void tap.test(
     await once(server, 'close');
   },
 );
+
+void tap.test('Uint8Array can be written to server', async (t) => {
+  t.plan(3);
+  const message = new Uint8Array([0, 1, 2]);
+
+  const server = net.createServer();
+  server.on('connection', (c) => {
+    c.setEncoding('binary');
+    c.on('data', (data) => {
+      t.equal(data, Buffer.from(message).toString());
+    });
+    c.on('end', () => {
+      server.close();
+    });
+  });
+
+  const address = await listenAndGetSocketAddress(server);
+
+  const socket = connect(`tcp://localhost:${address.port}`);
+
+  const writer = socket.writable.getWriter();
+  await t.resolves(writer.write(message));
+
+  await t.resolves(socket.close());
+
+  await once(server, 'close');
+});
+
+void tap.test('Uint8Array can be read from server', async (t) => {
+  t.plan(3);
+  const message = new Uint8Array([0, 1, 2]);
+
+  const server = net.createServer();
+  server.on('connection', (c) => {
+    c.setEncoding('binary');
+    c.on('data', () => {
+      c.write(message);
+    });
+    c.on('end', () => {
+      server.close();
+    });
+  });
+
+  const address = await listenAndGetSocketAddress(server);
+
+  const socket = connect(`tcp://localhost:${address.port}`);
+
+  const reader = socket.readable.getReader();
+  await t.resolves(socket.writable.getWriter().write('ready'));
+  await t.resolveMatch(reader.read(), message.buffer);
+
+  await t.resolves(socket.close());
+
+  await once(server, 'close');
+});
+
+void tap.test('Uint16Array can be written to server', async (t) => {
+  t.plan(3);
+  const message = new Uint16Array([0, 1, 2]);
+
+  const server = net.createServer();
+  server.on('connection', (c) => {
+    c.setEncoding('binary');
+    c.on('data', (data) => {
+      t.equal(data, Buffer.from(message.buffer).toString());
+    });
+    c.on('end', () => {
+      server.close();
+    });
+  });
+
+  const address = await listenAndGetSocketAddress(server);
+
+  const socket = connect(`tcp://localhost:${address.port}`);
+
+  const writer = socket.writable.getWriter();
+  await t.resolves(
+    writer.write(
+      new Uint8Array(message.buffer, message.byteOffset, message.byteLength),
+    ),
+  );
+
+  await t.resolves(socket.close());
+
+  await once(server, 'close');
+});
+
+void tap.test('Uint16Array can be read from server', async (t) => {
+  t.plan(3);
+  const message = new Uint16Array([0, 1, 2]);
+
+  const server = net.createServer();
+  server.on('connection', (c) => {
+    c.setEncoding('binary');
+    c.on('data', () => {
+      c.write(
+        new Uint8Array(message.buffer, message.byteOffset, message.byteLength),
+      );
+    });
+    c.on('end', () => {
+      server.close();
+    });
+  });
+
+  const address = await listenAndGetSocketAddress(server);
+
+  const socket = connect(`tcp://localhost:${address.port}`);
+
+  const reader = socket.readable.getReader();
+  await t.resolves(socket.writable.getWriter().write('ready'));
+  await t.resolveMatch(reader.read(), message.buffer);
+
+  await t.resolves(socket.close());
+
+  await once(server, 'close');
+});
+
+void tap.test('Uint32Array can be written to server', async (t) => {
+  t.plan(3);
+  const message = new Uint32Array([0, 1, 2]);
+
+  const server = net.createServer();
+  server.on('connection', (c) => {
+    c.setEncoding('binary');
+    c.on('data', (data) => {
+      t.equal(data, Buffer.from(message.buffer).toString());
+    });
+    c.on('end', () => {
+      server.close();
+    });
+  });
+
+  const address = await listenAndGetSocketAddress(server);
+
+  const socket = connect(`tcp://localhost:${address.port}`);
+
+  const writer = socket.writable.getWriter();
+  await t.resolves(
+    writer.write(
+      new Uint8Array(message.buffer, message.byteOffset, message.byteLength),
+    ),
+  );
+
+  await t.resolves(socket.close());
+
+  await once(server, 'close');
+});
+
+void tap.test('Uint32Array can be read from server', async (t) => {
+  t.plan(3);
+  const message = new Uint32Array([0, 1, 2]);
+
+  const server = net.createServer();
+  server.on('connection', (c) => {
+    c.setEncoding('binary');
+    c.on('data', () => {
+      c.write(
+        new Uint8Array(message.buffer, message.byteOffset, message.byteLength),
+      );
+    });
+    c.on('end', () => {
+      server.close();
+    });
+  });
+
+  const address = await listenAndGetSocketAddress(server);
+
+  const socket = connect(`tcp://localhost:${address.port}`);
+
+  const reader = socket.readable.getReader();
+  await t.resolves(socket.writable.getWriter().write('ready'));
+  await t.resolveMatch(reader.read(), message.buffer);
+
+  await t.resolves(socket.close());
+
+  await once(server, 'close');
+});
+
+void tap.test('BigUint64Array can be written to server', async (t) => {
+  t.plan(3);
+  const message = new BigUint64Array([0n, 1n, 2n]);
+
+  const server = net.createServer();
+  server.on('connection', (c) => {
+    c.setEncoding('binary');
+    c.on('data', (data) => {
+      t.equal(data, Buffer.from(message.buffer).toString());
+    });
+    c.on('end', () => {
+      server.close();
+    });
+  });
+
+  const address = await listenAndGetSocketAddress(server);
+
+  const socket = connect(`tcp://localhost:${address.port}`);
+
+  const writer = socket.writable.getWriter();
+  await t.resolves(
+    writer.write(
+      new Uint8Array(message.buffer, message.byteOffset, message.byteLength),
+    ),
+  );
+
+  await t.resolves(socket.close());
+
+  await once(server, 'close');
+});
+
+void tap.test('BigUint64Array can be read from server', async (t) => {
+  t.plan(3);
+  const message = new BigUint64Array([0n, 1n, 2n]);
+
+  const server = net.createServer();
+  server.on('connection', (c) => {
+    c.setEncoding('binary');
+    c.on('data', () => {
+      c.write(
+        new Uint8Array(message.buffer, message.byteOffset, message.byteLength),
+      );
+    });
+    c.on('end', () => {
+      server.close();
+    });
+  });
+
+  const address = await listenAndGetSocketAddress(server);
+
+  const socket = connect(`tcp://localhost:${address.port}`);
+
+  const reader = socket.readable.getReader();
+  await t.resolves(socket.writable.getWriter().write('ready'));
+  await t.resolveMatch(reader.read(), message.buffer);
+
+  await t.resolves(socket.close());
+
+  await once(server, 'close');
+});
